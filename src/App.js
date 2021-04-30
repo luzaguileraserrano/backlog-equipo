e944c6ae80d1ad7e4320d330f9abc41937e199f2
luzaguileraserrano <80918257+luzaguileraserrano@users.noreply.github.com>
2021-04-29 21:44:25 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+   <h1 className="title">
+      Servicios Gayosso <span aria-label="emoji" role="img">⚰️</span>
+   </h1>
+   <input type="text" className="new-task"/>
+</div>
+  );
+}
+export default App
