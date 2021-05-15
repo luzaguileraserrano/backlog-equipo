92b4146a02d751fbbf5556f93eeb3598816f41f9
luzaguileraserrano <80918257+luzaguileraserrano@users.noreply.github.com>
2021-05-15 13:33:26 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import imagen from './assets/img/imagen.png';
 import capillac from './assets/img/capillac.jpg';
 import capillav from './assets/img/capillav.jpg';
 import cementerio from './assets/img/cementerio.jpg';
+import caf from './assets/img/caf.jpg';
+import flor from './assets/img/flor.jpg';
 
 const App = _ =>{
 
@@ -22,8 +24,43 @@ const App = _ =>{
       <img src={capillav}/><br></br><br></br><br></br>
       <h2>Parque funeral colonias</h2><br></br><br></br>
       <img src={cementerio}/><br></br><br></br><br></br>
+      <h1>Prevision funeraria</h1><br></br><br></br>
+      <h2>Beneficios:</h2><br></br><br></br>
 
-        <p></p>
+        <h2>•	Contratos son 100% transferibles a familiares y amigos </h2><br></br>
+        <h2>•	Los únicos en ofrecer una oferta realmente integral,
+           servicio funerario + propiedades de descanso final</h2><br></br>
+        <h2>• El servicio de inhumación o cremación está incluido en el plan
+          </h2> <br></br><br></br> 
+        <h1>Plan de previsión de servicios funerarios</h1><br></br><br></br> 
+        <h2>•	Traslado a Casa Funeraria</h2><br></br>
+<h2>•	Servicios de Velación 24 hrs.</h2><br></br>
+<h2>•	Embalsamado y arreglo estético</h2><br></br>
+<h2>•	Capilla para servicios religiosos</h2><br></br>
+<h2>•	Asistencia personalizada</h2><br></br><br></br>
+<h1>Plan de Previsión de Mausoleo</h1><br></br><br></br>
+<h2>•	Servicio Funerario</h2><br></br>
+<h2>•	Servicio de Cremación</h2><br></br>
+<h2>•	Urna de cenizas a elección</h2><br></br>
+<h2>•	Nicho en Mausoleo Gayosso</h2><br></br>
+<h2>•	Derecho de Perpetuidad</h2><br></br><br></br>
+<h1>Plan de Previsión de Cementerio</h1><br></br><br></br>
+<h2>•	Servicio Funerario</h2><br></br>
+<h2>•	Lotes en Cementerio</h2><br></br>
+<h2>•	Derecho de Perpetuidad</h2><br></br>
+<h2>•	Embalsamado y arreglo estético</h2><br></br>
+<h2>•	Ceremonia de inhumación</h2><br></br><br></br><br></br>
+<h1>Cafetería punta del cielo</h1><br></br>
+<img src={caf}/><br></br><br></br>
+<h2>Contamos con cafetería Punta del Cielo en casi todas nuestras
+   agencias   y servicio de alimentos y bebidas en la sala de velación,
+    con barista.</h2><br></br><br></br>
+<h1>Florería</h1>
+<img src={flor}/><br></br><br></br>
+<h2>Todos nuestros arreglos se elaboran de manera personal y dedicada
+   por parte de floristas expertos, eligiendo las flores de la mejor
+    calidad y frescura para su entera satisfacción</h2><br></br>
+  <h3>Precio $ 3,085.00</h3><br></br><br></br>
          </center>
     </div>
     
