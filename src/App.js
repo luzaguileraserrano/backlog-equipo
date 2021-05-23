015d4e8d03c6c1e909a48cb3f5d8bc766f4c78a2
luzaguileraserrano <80918257+luzaguileraserrano@users.noreply.github.com>
2021-05-22 22:21:13 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -61,6 +61,67 @@ const App = _ =>{
    por parte de floristas expertos, eligiendo las flores de la mejor
     calidad y frescura para su entera satisfacción</h2><br></br>
   <h3>Precio $ 3,085.00</h3><br></br><br></br>
+  <h1>Historia de Gayosso</h1>
+  <h2>-1875 se establece la primera “Agencia de Inhumaciones Eusebio Gayosso y Cias”.
+    En las calles de San José del Real, hoy Isabel la Católica, esquina con 5 de mayo,
+     en el centro de la ciudad de México.</h2>
+  <h2>-1894, Don Eusebio recibe de manos del presidente Porfirio Díaz,
+     la patente de un sistema para enlutar cartas de aviso de defunción diseñado por él.</h2>
+  <h2>-1896, abre Sucursal 5 de mayo y Sucursal Hombres Ilustres (hoy Ave. Hidalgo)</h2>
+  <h2>-1902, se trasladaron las oficinas de Gayosso a un edificio estilo neoclásico,
+     en el terreno donde ahora se encuentra el Teatro Hidalgo.</h2>
+  <h2>-1916, Don Eusebio fallece a la edad de 62 años, 
+    dejando firmes los cimientos morales y una empresa con gran prestigio.</h2>
+  <h2>-1955, Abre sus puertas el primer velatorio, cuando la empresa ya tenía 80 años en operación.
+     Inició el servicio de velación en el número 273 de Paseo de la Reforma, entre las calles
+    Belgrado y Génova, con cuatro salas. Al mismo tiempo, se empezó la construcción de un local
+     en la calle de Sullivan, en la colonia San Rafael de la ciudad de México.</h2>
+  <h2>-1957, abre sucursal Sullivan.</h2>
+  <h2>-1960, se compra el terreno de Parque Funeral Colonias por el Sr. Hermilio López Sánchez.
+    Inicia operaciones en 1961, 1990 construcción del Crematorio y los primeros Nichos. </h2>
+  <h2>-60's, se funda Capillas del Carmen por el Sr. Julián Navarro en Guadalajara.</h2>
+  <h2>-1965, Para celebrar sus primeros 100 años Gayosso comienza la construcción de la Casa 
+    Funeraria más emblemática Félix Cuevas.</h2>
+  <h2>-1974, Inauguración del Primer Cementerio en Torreón. Cementerio Jardines del Parque 
+    S.A de C.V. siendo sus fundadores la familia Cantú Charles,pioneros laguneros en el ramo de 
+    Servicios de Inhumación y Funerarios.</h2>
+  <h2>-1976, se funda Capillas Vallarta por el Sr. Hermilio López Sánchez.</h2>
+  <h2>-1976, Construcción del primer Cementerio en León. Jardines de León S.A.</h2>
+  <h2>-1984, Construcción de primeras Capillas en Torreón y León.</h2>
+  <h2>-1988, apertura sucursal Cuernavaca.</h2>
+  <h2>-1989, apertura sucursal Sta. Mónica, col. Roma.</h2>
+  <h2>-1990, Adquisición de plazas en México, Tijuana, Morelia e Irapuato.</h2>
+  <h2>-1994, Los Arangoiz Gayosso Venden la Empresa a Stewart Enterprises, Inc.</h2>
+  <h2>-1995, diciembre, cambia su razón social a Jardines del Tiempo S.A. de C.V.,
+     estando como presidente municipal el Dr. Salvador Jalife García; creando así el Corporativo
+     y la Estandarización del Sistema.</h2>
+  <h2>-1995, Construcción del Segundo Cementerio en León. Jardines de León 2, constituido
+     el 13 de junio.</h2>
+  <h2>-1997, Se adquiere Mausoleos Samer, ahora Mausoleo Gayosso en León.</h2>
+  <h2>-1988, Adquisición de plazas en Guadalajara y Acapulco.</h2>
+  <h2>-2000, agosto, El Grupo Jardines del Tiempo adquiere las instalaciones de Parque Funeral 
+    Colonias,
+     así como Capillas Vallarta.</h2>
+  <h2>-2001, Jardines del Tiempo compra Gayosso a Stewart Enterprises.</h2>
+  <h2>-2001, Adquisición de Plaza Mexicali</h2>
+  <h2>-2005, Oficialmente recibe el nombre de Grupo Gayosso.</h2>
+  <h2>-2006, construcción del Mausoleo Gayosso en Guadalajara inaugurado por el Cardenal
+     Sandoval Iñiguez.</h2>
+  <h2>-2007, Advent Internacional, adquiere Grupo Gayosso.</h2>
+  <h2>-2008, Adquisición de Lomas Memorial en Santa Fé, DF.</h2>
+  <h2>-2008, Adquisición de Lomas Memorial en Santa Fé, DF.</h2>
+
+  <h2>Se implementa SAP ERP, para crecer y modernizarse para atender mejor las demandas del mercado
+    y afianzar su liderazgo en México.</h2>
+  <h2>-2009, Adquisición de Capillas Marianas en Monterrey.</h2><br></br><br></br>
+  <h1>Dato curioso</h1>
+  <h2>Con el afán de crear un Panteón de administración particular con base a las Leyes de Reforma,
+     que quitaba a la Iglesia el monopolio sobre el Registro Civil, el Servicio de Panteones y 
+     Cementerios, en 1872 es adquirido un lote en la Rotonda de los Hombres Ilustres por orden
+      del presidente Sebastián Lerdo de Tejada. Este lote fue donado por Juan Manuel Benfield y
+      su esposa Concepción Gayosso Mugarrieta (hermana de Don Eusebio). Ubicado en la Av.
+      Constituyentes en la Ciudad de México, se fundó el 4 de diciembre de 1874 con el nombre
+      de Panteón "Dolores" en honor a su donante Concepción Gayosso.  </h2>
          </center>
     </div>
     
