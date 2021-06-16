3d42102c01c914a8d9b79e4d9debe684c2ce079a
luzaguileraserrano <80918257+luzaguileraserrano@users.noreply.github.com>
2021-06-15 23:17:33 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import imagen from './assets/img/imagen.png';
-import capillac from './assets/img/capillac.jpg';
-import capillav from './assets/img/capillav.jpg';
-import cementerio from './assets/img/cementerio.jpg';
-import caf from './assets/img/caf.jpg';
-import flor from './assets/img/flor.jpg';
+import imagen from './assets/img/imagen.jpg';
 
 const App = _ =>{
 
@@ -15,114 +10,8 @@ const App = _ =>{
    
   
      <img src={imagen} /><br></br><br></br><br></br>
-    <h1>¡BIENVENIDO A GAYOSSO!</h1><br></br><h1>Conoce nuestras instalaciones en:</h1>
-    <br></br><br></br>
-    
-    <h2>Capillas del Carmen</h2><br></br><br></br>
-      <img src={capillac}/><br></br><br></br><br></br>
-      <h2>Capillas Vallarta</h2><br></br><br></br>
-      <img src={capillav}/><br></br><br></br><br></br>
-      <h2>Parque funeral colonias</h2><br></br><br></br>
-      <img src={cementerio}/><br></br><br></br><br></br>
-      <h1>Prevision funeraria</h1><br></br><br></br>
-      <h2>Beneficios:</h2><br></br><br></br>
 
-        <h2>•	Contratos son 100% transferibles a familiares y amigos </h2><br></br>
-        <h2>•	Los únicos en ofrecer una oferta realmente integral,
-           servicio funerario + propiedades de descanso final</h2><br></br>
-        <h2>• El servicio de inhumación o cremación está incluido en el plan
-          </h2> <br></br><br></br> 
-        <h1>Plan de previsión de servicios funerarios</h1><br></br><br></br> 
-        <h2>•	Traslado a Casa Funeraria</h2><br></br>
-<h2>•	Servicios de Velación 24 hrs.</h2><br></br>
-<h2>•	Embalsamado y arreglo estético</h2><br></br>
-<h2>•	Capilla para servicios religiosos</h2><br></br>
-<h2>•	Asistencia personalizada</h2><br></br><br></br>
-<h1>Plan de Previsión de Mausoleo</h1><br></br><br></br>
-<h2>•	Servicio Funerario</h2><br></br>
-<h2>•	Servicio de Cremación</h2><br></br>
-<h2>•	Urna de cenizas a elección</h2><br></br>
-<h2>•	Nicho en Mausoleo Gayosso</h2><br></br>
-<h2>•	Derecho de Perpetuidad</h2><br></br><br></br>
-<h1>Plan de Previsión de Cementerio</h1><br></br><br></br>
-<h2>•	Servicio Funerario</h2><br></br>
-<h2>•	Lotes en Cementerio</h2><br></br>
-<h2>•	Derecho de Perpetuidad</h2><br></br>
-<h2>•	Embalsamado y arreglo estético</h2><br></br>
-<h2>•	Ceremonia de inhumación</h2><br></br><br></br><br></br>
-<h1>Cafetería punta del cielo</h1><br></br>
-<img src={caf}/><br></br><br></br>
-<h2>Contamos con cafetería Punta del Cielo en casi todas nuestras
-   agencias   y servicio de alimentos y bebidas en la sala de velación,
-    con barista.</h2><br></br><br></br>
-<h1>Florería</h1>
-<img src={flor}/><br></br><br></br>
-<h2>Todos nuestros arreglos se elaboran de manera personal y dedicada
-   por parte de floristas expertos, eligiendo las flores de la mejor
-    calidad y frescura para su entera satisfacción</h2><br></br>
-  <h3>Precio $ 3,085.00</h3><br></br><br></br>
-  <h1>Historia de Gayosso</h1>
-  <h2>-1875 se establece la primera “Agencia de Inhumaciones Eusebio Gayosso y Cias”.
-    En las calles de San José del Real, hoy Isabel la Católica, esquina con 5 de mayo,
-     en el centro de la ciudad de México.</h2>
-  <h2>-1894, Don Eusebio recibe de manos del presidente Porfirio Díaz,
-     la patente de un sistema para enlutar cartas de aviso de defunción diseñado por él.</h2>
-  <h2>-1896, abre Sucursal 5 de mayo y Sucursal Hombres Ilustres (hoy Ave. Hidalgo)</h2>
-  <h2>-1902, se trasladaron las oficinas de Gayosso a un edificio estilo neoclásico,
-     en el terreno donde ahora se encuentra el Teatro Hidalgo.</h2>
-  <h2>-1916, Don Eusebio fallece a la edad de 62 años, 
-    dejando firmes los cimientos morales y una empresa con gran prestigio.</h2>
-  <h2>-1955, Abre sus puertas el primer velatorio, cuando la empresa ya tenía 80 años en operación.
-     Inició el servicio de velación en el número 273 de Paseo de la Reforma, entre las calles
-    Belgrado y Génova, con cuatro salas. Al mismo tiempo, se empezó la construcción de un local
-     en la calle de Sullivan, en la colonia San Rafael de la ciudad de México.</h2>
-  <h2>-1957, abre sucursal Sullivan.</h2>
-  <h2>-1960, se compra el terreno de Parque Funeral Colonias por el Sr. Hermilio López Sánchez.
-    Inicia operaciones en 1961, 1990 construcción del Crematorio y los primeros Nichos. </h2>
-  <h2>-60's, se funda Capillas del Carmen por el Sr. Julián Navarro en Guadalajara.</h2>
-  <h2>-1965, Para celebrar sus primeros 100 años Gayosso comienza la construcción de la Casa 
-    Funeraria más emblemática Félix Cuevas.</h2>
-  <h2>-1974, Inauguración del Primer Cementerio en Torreón. Cementerio Jardines del Parque 
-    S.A de C.V. siendo sus fundadores la familia Cantú Charles,pioneros laguneros en el ramo de 
-    Servicios de Inhumación y Funerarios.</h2>
-  <h2>-1976, se funda Capillas Vallarta por el Sr. Hermilio López Sánchez.</h2>
-  <h2>-1976, Construcción del primer Cementerio en León. Jardines de León S.A.</h2>
-  <h2>-1984, Construcción de primeras Capillas en Torreón y León.</h2>
-  <h2>-1988, apertura sucursal Cuernavaca.</h2>
-  <h2>-1989, apertura sucursal Sta. Mónica, col. Roma.</h2>
-  <h2>-1990, Adquisición de plazas en México, Tijuana, Morelia e Irapuato.</h2>
-  <h2>-1994, Los Arangoiz Gayosso Venden la Empresa a Stewart Enterprises, Inc.</h2>
-  <h2>-1995, diciembre, cambia su razón social a Jardines del Tiempo S.A. de C.V.,
-     estando como presidente municipal el Dr. Salvador Jalife García; creando así el Corporativo
-     y la Estandarización del Sistema.</h2>
-  <h2>-1995, Construcción del Segundo Cementerio en León. Jardines de León 2, constituido
-     el 13 de junio.</h2>
-  <h2>-1997, Se adquiere Mausoleos Samer, ahora Mausoleo Gayosso en León.</h2>
-  <h2>-1988, Adquisición de plazas en Guadalajara y Acapulco.</h2>
-  <h2>-2000, agosto, El Grupo Jardines del Tiempo adquiere las instalaciones de Parque Funeral 
-    Colonias,
-     así como Capillas Vallarta.</h2>
-  <h2>-2001, Jardines del Tiempo compra Gayosso a Stewart Enterprises.</h2>
-  <h2>-2001, Adquisición de Plaza Mexicali</h2>
-  <h2>-2005, Oficialmente recibe el nombre de Grupo Gayosso.</h2>
-  <h2>-2006, construcción del Mausoleo Gayosso en Guadalajara inaugurado por el Cardenal
-     Sandoval Iñiguez.</h2>
-  <h2>-2007, Advent Internacional, adquiere Grupo Gayosso.</h2>
-  <h2>-2008, Adquisición de Lomas Memorial en Santa Fé, DF.</h2>
-  <h2>-2008, Adquisición de Lomas Memorial en Santa Fé, DF.</h2>
-
-  <h2>Se implementa SAP ERP, para crecer y modernizarse para atender mejor las demandas del mercado
-    y afianzar su liderazgo en México.</h2>
-  <h2>-2009, Adquisición de Capillas Marianas en Monterrey.</h2><br></br><br></br>
-  <h1>Dato curioso</h1>
-  <h2>Con el afán de crear un Panteón de administración particular con base a las Leyes de Reforma,
-     que quitaba a la Iglesia el monopolio sobre el Registro Civil, el Servicio de Panteones y 
-     Cementerios, en 1872 es adquirido un lote en la Rotonda de los Hombres Ilustres por orden
-      del presidente Sebastián Lerdo de Tejada. Este lote fue donado por Juan Manuel Benfield y
-      su esposa Concepción Gayosso Mugarrieta (hermana de Don Eusebio). Ubicado en la Av.
-      Constituyentes en la Ciudad de México, se fundó el 4 de diciembre de 1874 con el nombre
-      de Panteón "Dolores" en honor a su donante Concepción Gayosso.  </h2>
-         </center>
+     </center>
     </div>
     
   )
