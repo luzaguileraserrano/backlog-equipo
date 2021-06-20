06b9e5b271b4a0cfc325671ad56ca80cf1e9ad15
luzaguileraserrano <80918257+luzaguileraserrano@users.noreply.github.com>
2021-06-20 00:05:33 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import imagen from './assets/img/imagen.jpg';
-
+import noviembre from './assets/img/noviembre.jpg';
+import carnaval from './assets/img/carnaval.jpg';
+import voladores from './assets/img/voladores.jpg';
 const App = _ =>{
 
   return   (
     <div >
       <center>
    
-  
-     <img src={imagen} /><br></br><br></br><br></br>
-
+    <br></br><br></br>
+    <h1>TRADICIONES Y COSTUMBRES MEXICANAS</h1><br></br><br></br>
+     <img src={imagen}width="400" />
+     <img src={noviembre}width="400" /><br></br>
+     <img src={carnaval}width="400" />
+     <img src={voladores}width="400" /><br></br><br></br>
      </center>
     </div>
     
